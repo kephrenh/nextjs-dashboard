4867207cc0bc1c46475cf99a79569d995cc6cbb2
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-05-01 11:44:32 +0200

    create home, customers, and invoices pages with navigation bar

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Kaushan_Script, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const kaushan = Kaushan_Script({ weight: '400', subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
